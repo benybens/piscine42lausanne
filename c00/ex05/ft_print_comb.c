47a40cef52c7838b25854ad52f89986c92b5b3f1
Yassine <yassine.benseghir@outlook.com>
2022-09-01 10:47:35 +0200

    add ex05

--- c00/ex05/ft_print_comb.c
+++ c00/ex05/ft_print_comb.c
@@ -14,7 +14,7 @@ void ft_putchar(char c)
          write(1,&c,1);
 }
 
-void ft_print_number(int d0,int d1,int d2)
+void ft_print_number(char d0,char d1,char d2)
 {
 	ft_putchar(d0);
 	ft_putchar(d1);
@@ -25,9 +25,23 @@ void ft_print_number(int d0,int d1,int d2)
 
 void ft_print_comb(void)
 {
-	int d0;
-	int d1;
-	int d2;
-
+	char d0 = '0';
+	char d1 = '1';
+	char d2 = '2';
 	
+	while(d0 <= '7')
+	{
+		while(d1 <= '8')
+		{
+			while(d2 <= '9')
+			{
+				ft_print_number(d0,d1,d2);
+				d2++;
+			}
+		d2 = d1 + 2;
+		d1++;
+		}
+	d1 = d0 + 1;
+	d0++;
+	}	
 }
