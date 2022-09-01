7dfb59f3bc91db20318c3c2c65596763673b81a2
Yassine <yassine.benseghir@outlook.com>
2022-09-01 11:26:06 +0200

    add ex05 start

--- c00/ex05/ft_print_comb.c
+++ c00/ex05/ft_print_comb.c
@@ -1,27 +1,33 @@
 #include <stdio.h>
 #include <unistd.h>
 
-void ft_is_negative(int n);
+void ft_print_comb(void);
+void ft_putchar(char c);
 
 int	main()
 {
-	int n;
-	scanf("%d",&n);
-	ft_is_negative(n);
+	ft_print_comb();
 }
 
-
-void ft_is_negative(int n)
+void ft_putchar(char c)
 {
-	char c;
+         write(1,&c,1);
+}
 
-	if(n < 0)
-	{
-		c = 'N';
-	}
-	else
-	{
-		c = 'P';
-	}
-	write(1,&c,1);
+void ft_print_number(int d0,int d1,int d2)
+{
+	ft_putchar(d0);
+	ft_putchar(d1);
+	ft_putchar(d2);
+	ft_putchar(',');
+	ft_putchar(' ');
+}
+
+void ft_print_comb(void)
+{
+	int d0;
+	int d1;
+	int d2;
+
+	
 }
