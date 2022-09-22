b09b66fd0d7e3df654569293865a6e8c30213a23
benybens <88706889+benybens@users.noreply.github.com>
2022-09-23 00:25:18 +0200

    update c00

--- Projects/c00/ex07/ft_putnbr.c
+++ Projects/c00/ex07/ft_putnbr.c
@@ -1,22 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ybensegh <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/22 22:30:50 by ybensegh          #+#    #+#             */
-/*   Updated: 2022/09/22 23:36:41 by ybensegh         ###   ########.fr       */
+/*   Created: 2022/09/22 23:36:17 by ybensegh          #+#    #+#             */
+/*   Updated: 2022/09/22 23:37:18 by ybensegh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdio.h>
-#include <unistd.h>
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
 
 void	ft_putnbr(int nbr)
 {
@@ -29,45 +21,4 @@ void	ft_putnbr(int nbr)
 		ft_putnbr(nbr / 10);
 		ft_putchar(nbr % 10 + '0');
 	}
-}
-
-void	print_del(void)
-{
-	ft_putchar(',');
-	ft_putchar(' ');
-}
-
-void	ft_print_comb2(void)
-{
-	int	a;
-	int	b;
-	int	bnew;
-
-	a = 0;
-	b = 1;
-	bnew = 1;
-	while (a < 99)
-	{
-		while (b < 100)
-		{
-			if (a < 10)
-				ft_putchar('0');
-			ft_putnbr(a);
-			ft_putchar(' ');
-			if (b < 10)
-				ft_putchar('0');
-			ft_putnbr(b);
-			if (a < 98)
-				print_del();
-			b++;
-		}
-		b = ++bnew;
-		a++;
-	}
-}
-
-int	main(void)
-{
-	ft_print_comb2();
-	return (0);
 }
